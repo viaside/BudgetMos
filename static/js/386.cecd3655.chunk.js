e3f20571ec023d27f97eb86fd4cda3630981b60f
Ivan <ivanvoloshiin@mail.ru>
2023-02-18 23:36:25 +0300

    Updates

---
+++ static/js/386.cecd3655.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[386],{386:function(p,e,u){u.r(e),e.default={}}}]);
+//# sourceMappingURL=386.cecd3655.chunk.js.map
---
+++ static/js/982.543838fc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[982],{982:function(p,e,u){u.r(e),e.default={}}}]);
+//# sourceMappingURL=982.543838fc.chunk.js.map
